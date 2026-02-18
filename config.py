661044d72b76e8799dd5d26b313fbc6e2b095952
MehrabChowdhury <93445792+FaysalMehrab@users.noreply.github.com>
2026-02-18 16:53:10 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+
+    'host' : 'localhost',
+    'port' : 3306,
+    'user' : 'root',
+    'password': 12123321,
+    'db' : 'testing_db',
+    'minsize': 1,
+    'maxsize' : 10,
+    
+}
---
+++ main.py
@@ -0,0 +1,3 @@
+import aiomysql
+import asyncio
+
